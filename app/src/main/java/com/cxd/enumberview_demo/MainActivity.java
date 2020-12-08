50116c9697d955b6dc8ecd1c1a9b41676cf2992a
bigdongdong <qipq@qq.com>
2020-12-08 17:21:25 +0800

    androidx -> support

--- app/src/main/java/com/cxd/enumberview_demo/MainActivity.java
+++ app/src/main/java/com/cxd/enumberview_demo/MainActivity.java
@@ -1,10 +1,11 @@
 package com.cxd.enumberview_demo;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
 
 import com.cxd.enumberview.ECharView;
 import com.cxd.enumberview.ENumberView;
